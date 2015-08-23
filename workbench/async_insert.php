4ebfb15b6c50c16d52f642e6f7761ac05a35ccf1
Ryan Brainard <brainard@heroku.com>
2015-08-23 00:16:08 -0700

    Async DML step switch and reset

--- workbench/async_insert.php
+++ workbench/async_insert.php
@@ -5,26 +5,44 @@ require_once 'controllers/AsyncDmlController.php';
 require_once 'header.php';
 
 $c = new AsyncDmlController();
+switch ($c->stepView()) {
+
+case AsyncDmlController::STEP_UPLOAD:
 ?>
+    <form method="post" enctype="multipart/form-data" action="">
+        <?php print getCsrfFormTag(); ?>
+        <div>
+            <label>
+                Object Type:
+                <?php printObjectSelection(WorkbenchContext::get()->getDefaultObject()); ?>
+            </label>
+        </div>
+        <!-- TODO: single file look up -->
+        <div>
+            <label>
+                From File
+                <input type="file" name="<?php echo AsyncDmlController::FILE_KEY ?>"/>
+            </label>
+        </div>
+        <div>
+            <input type="submit" value="Next"/>
+        </div>
+    </form>
 
-<form method="post" enctype="multipart/form-data" action="">
-    <?php print getCsrfFormTag(); ?>
-    <div>
-        <label>
-            Object Type:
-            <?php printObjectSelection(WorkbenchContext::get()->getDefaultObject()); ?>
-        </label>
-    </div>
-    <!-- TODO: single file look up -->
-    <div>
-        <label>
-            From File
-            <input type="file" name="<?php echo AsyncDmlController::CSV_FILE ?>"/>
-        </label>
-    </div>
-    <div>
-        <input type="submit" value="Next"/>
-    </div>
-</form>
+<?php break; case AsyncDmlController::STEP_MAP: ?>
+    <form method="post" action="">
+        <?php print getCsrfFormTag(); ?>
 
-<?php require_once 'footer.php'; ?>
+        <?php var_dump($c->getColumnNames()); // TODO: expand into table ?>
+
+        <div>
+            <input type="submit" name="<?php echo AsyncDmlController::RESET_KEY ?>" value="Reset"/>
+            <input type="submit" value="Next"/>
+        </div>
+    </form>
+
+
+<?php
+break; } // end step switch
+require_once 'footer.php';
+?>
--- workbench/controllers/AsyncDmlController.php
+++ workbench/controllers/AsyncDmlController.php
@@ -2,18 +2,39 @@
 
 class AsyncDmlController {
     const SESSION_KEY = 'ASYNC_DML';
-    const CSV_FILE = 'CSV_FILE';
+    const FILE_KEY = 'CSV_FILE';
+    const RESET_KEY = 'RESET';
     const CHUNK_SIZE = 1024;
     const FILE_EXP_SEC = 10 * 60;
     const COLUMN_NAMES = 'COLUMN_NAMES';
 
+    const STEP_UPLOAD = 'STEP_UPLOAD';
+    const STEP_MAP = 'STEP_MAP';
+
     function __construct(){
-        if (array_key_exists(self::CSV_FILE, $_FILES)) {
-            $handle = fopen($_FILES[self::CSV_FILE]['tmp_name'], 'rb');
+        if (isset($_POST[self::RESET_KEY])) {
+            $this->reset();
+        }
+
+        if (isset($_FILES[self::FILE_KEY])) {
+            $handle = fopen($_FILES[self::FILE_KEY]['tmp_name'], 'rb');
             $this->setColumnNames($handle);
-            $this->stashCsvFile($handle);
+            $this->stashFile($handle);
             fclose($handle);
         }
+    }
+
+    public function stepView() {
+        if ($this->isFileStashed() && $this->getColumnNames()) { // TODO: something more than this? how to clear?
+            return self::STEP_MAP;
+        } else {
+            return self::STEP_UPLOAD;
+        }
+    }
+
+    private function reset() {
+        $this->clearFile();
+        $this->clearSession();
     }
 
     private function setColumnNames($handle) {
@@ -21,25 +42,29 @@ class AsyncDmlController {
         $this->writeSession(self::COLUMN_NAMES, str_getcsv(fgets($handle)));
     }
 
-    private function getColumnNames($handle) {
-        $this->readSession(self::COLUMN_NAMES);
+    public function getColumnNames() {
+        return $this->readSession(self::COLUMN_NAMES);
     }
 
-    private function csvFileSessionKey() {
-        return self::CSV_FILE . ":" . session_id();
+    private function fileSessionKey() {
+        return self::FILE_KEY . ":" . session_id();
     }
 
-    private function stashCsvFile($handle) {
-        $this->stashFile($this->csvFileSessionKey(), $handle);
+    private function isFileStashed() {
+        return redis()->exists($this->fileSessionKey());
     }
 
-    private function stashFile($key, $handle) {
+    private function stashFile($handle) {
         rewind($handle);
-        redis()->setex($key, self::FILE_EXP_SEC, null);
+        redis()->setex($this->fileSessionKey(), self::FILE_EXP_SEC, null);
         while (!feof($handle)) {
             $chunk = fread($handle, self::CHUNK_SIZE);
-            redis()->append($key, $chunk);
+            redis()->append($this->fileSessionKey(), $chunk);
         }
+    }
+
+    private function clearFile() {
+        redis()->del($this->fileSessionKey());
     }
 
     private function writeSession($subkey, $value) {
@@ -48,5 +73,9 @@ class AsyncDmlController {
 
     private function readSession($subkey) {
         return $_SESSION[self::SESSION_KEY][$subkey];
+    }
+
+    private function clearSession() {
+        unset($_SESSION[self::SESSION_KEY]);
     }
 }
